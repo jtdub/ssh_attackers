24b782c27260f2707f928bda04c5dee948a41645
James Williams <amascuba@gmail.com>
2015-03-03 01:36:26 -0500

    pep8

--- ssh_attackers.py
+++ ssh_attackers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from ipwhois import IPWhois
 
 
@@ -13,7 +14,7 @@ def get_attacker_info(ip_addr):
 
 
 if __name__ == "__main__":
-    log_file = open('/var/log/denyhosts','r')
+    log_file = open('/var/log/denyhosts', 'r')
     lookup_list = []
     denied_host_count = 0
     for log_entry in iter(log_file):
@@ -22,17 +23,17 @@ if __name__ == "__main__":
     log_file.close()
     print "Total Denied Hosts: %s" % denied_host_count
 
-    log_file = open('/var/log/denyhosts','r')
+    log_file = open('/var/log/denyhosts', 'r')
     for log_entry in iter(log_file):
         if 'new denied hosts' in log_entry:
             date = log_entry.split(',')
             date = date[0]
             ip_address = log_entry.split(' ')[-1]
             ip_address = ip_address.strip('\'[').split('\'')[0]
-	    attacker_info = get_attacker_info(ip_address) 
-	    attacker_info['blacklist_date'] = date
-	    lookup_list.append(attacker_info)
-	    print attacker_info 
+            attacker_info = get_attacker_info(ip_address)
+            attacker_info['blacklist_date'] = date
+            lookup_list.append(attacker_info)
+            print attacker_info
     log_file.close()
 
-    #print lookup_list
+    # print lookup_list
